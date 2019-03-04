9af12540704bfbb713408bca900ccb840f69bbc8
ClaraMFlood <44729357+ClaraMFlood@users.noreply.github.com>
2019-03-03 20:13:19 -0500

    updating to fix problematic pine tree

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // GOAL: Proportional symbols representing attribute values of mapped features
 
-var currentMap = 0 // Global variable to hold the leaflet map object
+var currentMap = 0; // Global variable to hold the leaflet map object
 
 // Function to instantiate the Leaflet map
 function createMap(){
@@ -11,7 +11,7 @@ function createMap(){
         zoom: 4,
         //set to false to overide default position
         zoomControl: false
-    })
+    });
 
     // add zoomControl
     L.control.zoom({ position: 'topright' }).addTo(map);
@@ -23,31 +23,31 @@ function createMap(){
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> | Visitor data: <a href="https://irma.nps.gov/Stats/">National Park Service</a> | Contributors: <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a> | Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
         id: 'cmflood',
-    }).addTo(map)
+    }).addTo(map);
     
     // Set global map variable to map object
-    currentMap = map
+    currentMap = map;
 
     //call getData function
-    getData(map)
-}
+    getData(map);
+};
 
 
 
 
 // Initialize global variables for use later
-var currentLayer = 0 // holds geoJsonLayer for future modifications
-var currentAttributes = 0 // Visitor attribute names
-var currentAttributes2 = 0 // Emptn attribute names
-var currentFilter = 'all' // current Filter selection, initially 'all'
-var rawJson = 0 // holds ajax response, aka raw json data
-var featureSelected = 0 // holds the currently selected park information
-var currentAttribute = 0 // holds the currently selected visitor attribute name
-var currentAttribute2 = 0 // empty attribute name
+var currentLayer = 0; // holds geoJsonLayer for future modifications
+var currentAttributes = 0; // Visitor attribute names
+var currentAttributes2 = 0; // Emptn attribute names
+var currentFilter = 'all'; // current Filter selection, initially 'all'
+var rawJson = 0; // holds ajax response, aka raw json data
+var featureSelected = 0; // holds the currently selected park information
+var currentAttribute = 0; // holds the currently selected visitor attribute name
+var currentAttribute2 = 0; // empty attribute name
 
 function numberWithCommas(x) {
    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
+};
 
 // Function to retrieve the data via ajax and place it on the map
 function getData(map){
@@ -56,25 +56,23 @@ function getData(map){
         dataType: "json",
         success: function(response){
             // Set global rawJson to ajax response
-            rawJson = response
+            rawJson = response;
             // Process rawJson/response into lists of data sets- Percentage and Population
-            var processedAttributes = processData(response)
-            currentAttributes = processedAttributes[0]
-            console.log(currentAttributes[0])
-            currentAttributes2 = processedAttributes[1]
+            var processedAttributes = processData(response);
+            currentAttributes = processedAttributes[0];
+            currentAttributes2 = processedAttributes[1];
             // Call function to create proportional symbols, put in a layer
-            geoJsonLayer = createPropSymbols(response, map, 0, currentFilter)
-            currentLayer = geoJsonLayer
+            geoJsonLayer = createPropSymbols(response, map, 0, currentFilter);
+            currentLayer = geoJsonLayer;
             // Call function to create sequence controls for user
-            createSequenceControls(map, currentAttributes)
+            createSequenceControls(map, currentAttributes);
                     
             //add geo JSON layer to map
-            map.addLayer(geoJsonLayer)
+            map.addLayer(geoJsonLayer);
             
             // call function to create proportional symbol legend, must be called AFTER the above addLayer is called so that a layer already exists!
-            createLegend(map, currentAttributes)
-            console.log(currentAttributes)
-            updateLegend(map, currentAttributes[0])
+            createLegend(map, currentAttributes);
+            updateLegend(map, currentAttributes[0]);
         }
     })
 }
@@ -82,10 +80,10 @@ function getData(map){
 // Build attributes arrays from the data
 function processData(data){
     // Empty arrays to hold attributes
-    var attributes = []
-    var attributes2 = []
+    var attributes = [];
+    var attributes2 = [];
     // Properties of the first feature in the dataset
-    var properties = data.features[0].properties
+    var properties = data.features[0].properties;
     // Push each attribute name into attributes array
     for (var attribute in properties){
         //catalog attributes with percentage values
@@ -96,7 +94,7 @@ function processData(data){
         if (attribute.indexOf("Visitors") > -1){
             attributes2.push(attribute)
         }
-    }
+    };
     return [attributes, attributes2];
 };
     
@@ -122,28 +120,28 @@ function createPropSymbols(data, map, idx, filterStr) {
         pointToLayer: function (feature, latlng){
             
             // For each feature, determine its value for the selected attribute
-            var attValue = Number(feature.properties[attribute])
+            var attValue = Number(feature.properties[attribute]);
             
             // Test for variable type, then give each feature's circle marker a radius based on its attribute value 
             
-            var strTest = attribute.search("Visitors")
+            var strTest = attribute.search("Visitors");
             if (strTest > -1) {
                 geojsonMarkerOptions.radius = calcPropRadius(attValue, 0.0008)
                 details = ["Population", ""]
                 details2 = ["Percentage", "%"] //remove?
-            } 
+            };
             
             // Create circle marker layer
-            var layer = L.circleMarker(latlng, geojsonMarkerOptions)
+            var layer = L.circleMarker(latlng, geojsonMarkerOptions);
             
             // Build popup content string
-            var popupContent = "<p><b>Park:</b> " + feature.properties.ParkName + "</p>"
+            var popupContent = "<p><b>Park:</b> " + feature.properties.ParkName + "</p>";
             
             // Bind the popup to the circle marker
             layer.bindPopup(popupContent, {
                 offset: new L.Point(0, -geojsonMarkerOptions.radius),
                 closeButton: false,
-            })
+            });
             
             // Event listeners to open popup on hover, update the info panel on click and
             // Add clicked feature to global variable for future use
@@ -159,14 +157,14 @@ function createPropSymbols(data, map, idx, filterStr) {
                     updatePanel(currentAttribute, details)
                     this.closePopup();
                 }
-            })
+            });
 
-            return layer
+            return layer;
         },
         filter: function(feature, layer) { // Add filter function for new geoJsonLayer
             // If the data-filter attribute is set to "all", return all (true)
             // Otherwise, filter markers based on visitor size
-            var returnBool = false
+            var returnBool = false;
             if (filterStr === 'all'){
                 returnBool = true
             } else if (filterStr === 'big'){
@@ -182,36 +180,36 @@ function createPropSymbols(data, map, idx, filterStr) {
                     returnBool = true
                 }
             } 
-            return returnBool
+            return returnBool;
         }
-    }
+    };
     // Create a Leaflet GeoJSON layer, based on rawJson data and previously defined geoJsonOptions
-    var geoJsonLayer = L.geoJson(data, geoJsonOptions)
-    return geoJsonLayer    
+    var geoJsonLayer = L.geoJson(data, geoJsonOptions);
+    return geoJsonLayer; 
 };
 
 // Calculate the radius of each proportional symbol
 function calcPropRadius(attValue, scaleFactor) {
     // Area based on attribute value and scale factor
-    var area = attValue * scaleFactor
+    var area = attValue * scaleFactor;
     // Radius calculated based on area
-    var radius = Math.sqrt(area/Math.PI)
-    return radius
-}
+    var radius = Math.sqrt(area/Math.PI);
+    return radius;
+};
 
 // Create new sequence controls
 function createSequenceControls(map, attributes){
     
     // Add skip button for "reverse" functionality, replace button content with image
-    $('#slider').append('<button class="skip" id="reverse">Reverse</button>')  
-    $('#reverse').html('<img src="img/arrowback5.svg" alt= "Arrow back" title = "Back">')
+    $('#slider').append('<button class="skip" id="reverse">Reverse</button>');  
+    $('#reverse').html('<img src="img/arrowback5.svg" alt= "Arrow back" title = "Back">');
     
     // Create range input element (slider)
-    $('#slider').append('<input class="range-slider" type="range">')
+    $('#slider').append('<input class="range-slider" type="range">');
     
     // Add skip button for "forward" functionality, replace button content with image
-    $('#slider').append('<button class="skip" id="forward">Skip</button>')    
-    $('#forward').html('<img src="img/arrowforward5.svg" alt = "Arrow forward" title = "Forward">')
+    $('#slider').append('<button class="skip" id="forward">Skip</button>');    
+    $('#forward').html('<img src="img/arrowforward5.svg" alt = "Arrow forward" title = "Forward">');
     
     // Set slider attributes
     $('.range-slider').attr({
@@ -219,12 +217,12 @@ function createSequenceControls(map, attributes){
         min: 0,
         value: 0,
         step: 1
-    })
+    });
     
     // Click listener for buttons
     $('.skip').click(function(){
         // Get the old index value
-        var index = $('.range-slider').val()
+        var index = $('.range-slider').val();
 
         // Increment or decrement depending on button clicked
         if ($(this).attr('id') == 'forward'){
@@ -238,51 +236,50 @@ function createSequenceControls(map, attributes){
         }
 
         // Update slider after arrow click
-        $('.range-slider').val(index)
+        $('.range-slider').val(index);
         
         // Rebuild layer as population values may have changed during time sequence
-        currentMap.removeLayer(geoJsonLayer)
-        geoJsonLayer = createPropSymbols(rawJson, currentMap, index, currentFilter)
-        currentLayer = geoJsonLayer
-        currentMap.addLayer(geoJsonLayer)
+        currentMap.removeLayer(geoJsonLayer);
+        geoJsonLayer = createPropSymbols(rawJson, currentMap, index, currentFilter);
+        currentLayer = geoJsonLayer;
+        currentMap.addLayer(geoJsonLayer);
         
         // Reset current attributes and update the info panel
-        currentAttribute = currentAttributes[index]
-        console.log("here's the sequence click currentAttribute[index] " + currentAttribute)
-        updatePanel()
+        currentAttribute = currentAttributes[index];
+        updatePanel();
         
         // Update legend title with new year
-        $('#legendTitle').text("Visitors in " + currentAttribute.split("_")[1])
-        updateLegend(map, currentAttribute)
-    })
+        $('#legendTitle').text("Visitors in " + currentAttribute.split("_")[1]);
+        updateLegend(map, currentAttribute);
+    });
 
     // Input listener for slider
     $('.range-slider').on('input', function(){
         // Get the new index value
-        var index = $(this).val()
+        var index = $(this).val();
         
         // Rebuild layer as population values may have changed during time sequence
-        currentMap.removeLayer(geoJsonLayer)
-        geoJsonLayer = createPropSymbols(rawJson, currentMap, index, currentFilter)
-        currentLayer = geoJsonLayer
-        currentMap.addLayer(geoJsonLayer)
+        currentMap.removeLayer(geoJsonLayer);
+        geoJsonLayer = createPropSymbols(rawJson, currentMap, index, currentFilter);
+        currentLayer = geoJsonLayer;
+        currentMap.addLayer(geoJsonLayer);
         
         // Reset current attributes and update the info panel
-        currentAttribute = attributes[index]
-        updatePanel()
+        currentAttribute = attributes[index];
+        updatePanel();
         
         // Update legend title with new year
-        $('#legendTitle').text("Visitors in " + currentAttribute.split("_")[1])
-        updateLegend(map, currentAttribute)
-    })
-}
+        $('#legendTitle').text("Visitors in " + currentAttribute.split("_")[1]);
+        updateLegend(map, currentAttribute);
+    });
+};
 
 // Function to update the information panel
 function updatePanel() {
     if (featureSelected != 0) {
         
         // Determine the nature of the main variable
-        var strTest = currentAttribute.search("Visitors")
+        var strTest = currentAttribute.search("Visitors");
         
         if (strTest > -1) {
             details = ["Visitors", ""]
@@ -296,32 +293,32 @@ function updatePanel() {
         panelContent += "<p><b>" + details[0] + " in " + year + ":</b> " + visitsWithCommas + "</p>"
 
         // Update panel with new popup content
-        $("#panel").html(panelContent)
-    }
-}
+        $("#panel").html(panelContent);
+    };
+};
 
 // Click listener for the filter menu
 $('.menu-ui a').on('click', function() {
     // For each filter link, get the 'data-filter' attribute value.
-    var filter = $(this).data('filter')
+    var filter = $(this).data('filter');
     
     // Set global variable for future use
-    currentFilter = filter
+    currentFilter = filter;
     
     // Change which filter menu option is active, get slider index
-    $(this).addClass('active').siblings().removeClass('active')
-    var currentIdx = $('.range-slider').val()
+    $(this).addClass('active').siblings().removeClass('active');
+    var currentIdx = $('.range-slider').val();
     
     // Remove current map layer, create new one with appropriate filter
-    currentMap.removeLayer(geoJsonLayer)
-    geoJsonLayer = createPropSymbols(rawJson, currentMap, currentIdx, filter)
-    currentLayer = geoJsonLayer
+    currentMap.removeLayer(geoJsonLayer);
+    geoJsonLayer = createPropSymbols(rawJson, currentMap, currentIdx, filter);
+    currentLayer = geoJsonLayer;
     
     // Add new layer to map, wipe away the info panel
-    currentMap.addLayer(geoJsonLayer)
+    currentMap.addLayer(geoJsonLayer);
     updateLegend(currentMap, currentAttribute);
-    $("#panel").html("<p>This is a map of the most popular, well-visited National Parks in the United States. Want to know more? Click on one of the parks!</p>")
-})
+    $("#panel").html("<p>This is a map of the most popular, well-visited National Parks in the United States. Want to know more? Click on one of the parks!</p>");
+});
 
 function createLegend(map, attributes){
     //console.log("hellohello " + attributes);
@@ -437,7 +434,7 @@ function updateLegend(map, attribute){
     
 };
 
-$(document).ready(createMap)
+$(document).ready(createMap);
 
 
 
